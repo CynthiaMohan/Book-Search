e24b63b08bab1ddf742aa3f5404f7069c9d38096
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-24 17:04:08 -0700

    Set up Apollo Server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Book Search Engine Starter Code
+# Book Search
+
+# Description
+In this project take a fully functioning Google Books API search engine built with a RESTful API, and refactor it to be a GraphQL API built with Apollo Server. The app was built using the MERN stack, with a React front end, MongoDB database, and Node.js/Express.js server and API.
--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+//import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create a new apollo server and pass the schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//integrate the Apollo server with Express
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +32,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  //log where we go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });

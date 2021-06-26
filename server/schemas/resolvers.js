b3bfa88560425ac1cee00db5b6df1f65298ad3a6
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-25 18:00:28 -0700

    Added mutations for addUser and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,38 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 
-const resolvers = () => {
+const resolvers = {
+    Query: {
+        //get all USers
+        me: async () => {
+            const usersList = await User.find()
+                .select('-__v -password')
+                .populate('books');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const newUser = await User.create(args);
+            return newUser;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect email');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password');
+            }
+            return user;
+        },
+        saveBook: async () => {
 
-};
+        },
+        removeBook: async () => {
+
+        }
+    }
+}
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,16 +12,28 @@ type Book{
     _id:ID!
     bookId:String
     authors:[String]
-    # authors:String
     description:String
     title:String
     image:String
     link:String
-    
+}
+input savedBook{
+    authors:[String]
+    description:String
+    title:String
+    bookId:String
+    image:String
+    link:String
 }
 type Query{
-    me:User
+    me: User
+}
+type Mutation{
+    login(email:String!,password:String!):User
+    addUser(username:String!,email:String!,password:String!):User
+    saveBook(input:savedBook!):User
+    removeBook(bookId:ID):User
 }
 `;
 
-module.exports=typeDefs;
+module.exports = typeDefs;

22a01b87810a558ca51625e1d67662c3964fbe63
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-29 13:19:27 -0700

    Login works

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,6 +14,7 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
 mutation addUser($username:String!,$email:String!,$password:String!){
     addUser(username:$username,email:$email,password:$password){
+        token
         user{
             _id
             username
@@ -28,7 +29,6 @@ mutation addUser($username:String!,$email:String!,$password:String!){
                 link
             }
         }
-        token
     }
 }
 `;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,17 +6,21 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    console.log("hi");
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
+      console.log(token);
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      console.log("There is no token");
+      // return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -25,12 +29,14 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
+    // next();
+    return req;
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
